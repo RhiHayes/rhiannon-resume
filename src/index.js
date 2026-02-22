d23a81ad68e1fa45ec10a8bd3bbfe31015408836
Rhiannon Hayes <rhiannon.nicole.hayes@gmail.com>
2026-02-21 18:20:42 -0600

    Edits

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <HashRouter>
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}/>
+    </Routes>
   </HashRouter>
+  </React.StrictMode>
 );
